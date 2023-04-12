d02a85a02b762bc1d35e2c6a9cef5f9b23dafc41
Gaet <scorpiogaet@gmail.com>
2023-04-12 17:56:25 +0200

    primi test

--- src/test/java/it/isa/progetto/AppTest.java
+++ src/test/java/it/isa/progetto/AppTest.java
@@ -1,7 +1,18 @@
 package it.isa.progetto;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import org.junit.After;
+import org.junit.AfterClass;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.fail;
+import org.junit.Before;
+import org.junit.BeforeClass;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeAll;
@@ -17,6 +28,36 @@ public class AppTest
     /**
      * Rigorous Test :-)
      */
+    public static Connection conn = null;
+    
+    @BeforeAll
+    public static void iniziaConnessione(){
+        System.out.print("inizializzo connessione per i test...");
+        try {
+            Class.forName("com.ibm.db2.jcc.DB2Driver");
+        }
+        catch (ClassNotFoundException e) {
+            System.out.println("Please include Classpath  Where your DB2 Driver is located");
+            e.printStackTrace();
+            return;
+        }
+        System.out.println("DB2 driver is loaded successfully");
+        try{
+        conn = DriverManager.getConnection("jdbc:db2://localhost:50000/isa:retrieveMessagesFromServerOnGetMessage=true;","db2inst1","pass");
+        }catch(SQLException e){
+            System.out.println("DB2 Database connection Failed");
+            e.printStackTrace();
+            return;
+        }
+            
+    }
+    
+    @Test
+    public void testPDip1(){
+        App a = new App();
+        a.pDip1(conn);
+    }
+    
     @Test
     public void shouldAnswerWithTrue()
     {
